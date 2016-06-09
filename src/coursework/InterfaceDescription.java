e0874ec3fe834ed538f5e0a40f3d82bc56099e22
Анастасия Поцелуйко < >
2016-06-09 12:14:43 +0300

    Добавлена новая онтология, исправлен класс решений задачи.

--- src/coursework/InterfaceDescription.java
+++ src/coursework/InterfaceDescription.java
@@ -12,11 +12,14 @@ import jcolibri.datatypes.Instance;
  * @author Анастасия
  */
 public class InterfaceDescription implements jcolibri.cbrcore.CaseComponent {
-    public Instance CARCASSESTYPE;
-    public Instance MARK;
-    public Instance TRACK;
-    public Instance WEATHER;
-    public Instance TIRESTYPE;
+    public Instance USER_VIDEO_CAMERA;
+    public Instance USER_AUDIO_COMPONENT;
+    public Instance USER_MICROPHONE;
+    public Instance USER_SPECIAL_DEVICE;
+    public Instance USER_MAIN_DEVICE;
+    public Instance USER_DISEASE_RANGE;
+    public Instance USER_DISEASE;
+    public Instance USER_AGE;
     public Instance CASEID;
     
     @Override
@@ -26,50 +29,75 @@ public class InterfaceDescription implements jcolibri.cbrcore.CaseComponent {
     public Instance getCASEID() {
         return CASEID;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    public Instance getCARCASSESTYPE() {
-        return CARCASSESTYPE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_VIDEO_CAMERA() {
+        return USER_VIDEO_CAMERA;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public Instance getTIRESTYPE() {
-        return TIRESTYPE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_AUDIO_COMPONENT() {
+        return USER_AUDIO_COMPONENT;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    public Instance getMARK() {
-        return MARK;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public Instance getTRACK() {
-        return TRACK;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_MICROPHONE() {
+        return USER_MICROPHONE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public Instance getWEATHER() {
-        return WEATHER;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_SPECIAL_DEVICE() {
+        return USER_SPECIAL_DEVICE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setMARK(Instance mark) {
-        MARK = mark;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_MAIN_DEVICE() {
+        return USER_MAIN_DEVICE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public Instance getUSER_DISEASE_RANGE() {
+        return USER_DISEASE_RANGE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setWEATHER(Instance weather) {
-        WEATHER = weather;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_DISEASE() {
+        return USER_DISEASE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setTIRESTYPE(Instance tirestype) {
-        TIRESTYPE = tirestype;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Instance getUSER_AGE() {
+        return USER_AGE;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setCARCASSESTYPE(Instance carcasessestype) {
-        CARCASSESTYPE = carcasessestype;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setUSER_VIDEO_CAMERA(Instance videoCamera) {
+        USER_VIDEO_CAMERA = videoCamera;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setTRACK(Instance track) {
-        TRACK = track;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void setUSER_AUDIO_COMPONENT(Instance audioComponent) {
+        USER_AUDIO_COMPONENT = audioComponent;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setUSER_MICROPHONE(Instance microphone) {
+        USER_MICROPHONE = microphone;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setUSER_SPECIAL_DEVICE(Instance specialDevice) {
+        USER_SPECIAL_DEVICE = specialDevice;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setUSER_MAIN_DEVICE(Instance mainDevice) {
+        USER_MAIN_DEVICE = mainDevice;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void setUSER_DISEASE_RANGE(Instance diseaseRange) {
+        USER_DISEASE_RANGE = diseaseRange;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setUSER_DISEASE(Instance disease) {
+        USER_DISEASE = disease;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setUSER_AGE(Instance age) {
+        USER_AGE = age;//throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public void setCASEID(Instance caseId) {
         CASEID = caseId;// throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public String toString() {
-        return "("+ CASEID + ";"+ TRACK + ";"+ MARK + ";"+ TIRESTYPE + ";"+ CARCASSESTYPE + ";"+ WEATHER + ")";
+        return "("+ CASEID + ";"+ USER_AGE + ";"+ USER_AUDIO_COMPONENT 
+                + ";"+ USER_DISEASE + ";"+ USER_DISEASE_RANGE + ";"+ USER_MAIN_DEVICE 
+                + ";"+ USER_MICROPHONE + ";"+ USER_SPECIAL_DEVICE + ";"+ USER_VIDEO_CAMERA + ")";
     }
     
 }
